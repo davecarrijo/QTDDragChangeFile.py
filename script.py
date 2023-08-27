4eb563fe81abd6cab67a1cd752241b281f9280d0
David Carrijø <49088508+davecarrijo@users.noreply.github.com>
2023-08-27 09:56:44 +0100

    update the drag and drop start

--- script.py
+++ script.py
@@ -1,8 +1,66 @@
 import sys
-from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget
+import os
+import typing
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget, QApplication, QVBoxLayout
 from PyQt5.QtWidgets import QPushButton
-from PyQt5.QtCore import QSize
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtGui import QPixmap
+
+
+class ImageLabel(QLabel):
+    def __init__(self):
+        super().__init__()
+
+        self.setAlignment(Qt.AlignCenter)
+        self.setText('\n\n Drop image here \n\n')
+        self.setStyleSheet('''
+                           Qlabel{
+                                border: 4px dashed #aaa
+                           }
+                          ''')
+
+    def setPixmap(self, image):
+        super().setPixmap(image)
+
+
+class DragDrop(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.resize(400, 400)
+        self.setAcceptDrops(True)
+
+        mainLayout = QVBoxLayout()
+
+        self.photoViewer = ImageLabel()
+        mainLayout.addWidget(self.photoViewer)
+
+        self.setLayout(mainLayout)
+
+        def dragEnterEvent(self, event):
+            if event.mineData().hasImage:
+                event.accept()
+            else:
+                event.ignore()
+
+        def dragMoveEvent(self, event):
+            if event.mineData().hasImage:
+                event.accept()
+            else:
+                event.ignore()
+
+        def dropEvent(self, event):
+            if event.mineData().hasImage:
+                event.setDropAction(Qt.CopyAction)
+                file_path = event.mineData().urls()[0].toLocalFile()
+                self.set_image(file_path)
+
+                event.accept()
+            else:
+                event.ignore()
+
+        def set_image(self, file_path):
+            self.photoViewer.setPixmap(QPixmap(file_path))
 
 
 class MainWindow(QMainWindow):
@@ -17,25 +75,29 @@ class MainWindow(QMainWindow):
         pybutton.clicked.connect(self.clickMethod)
         pybutton.resize(100, 32)
         pybutton.move(5, 5)
-        pybutton.setStyleSheet("background-color : none;")
+        pybutton.setStyleSheet("background-color : none")
 
         secPyButton = QPushButton("Open File", self)
         secPyButton.clicked.connect(self.clickMethod)
         secPyButton.resize(100, 32)
         secPyButton.move(110, 5)
-        secPyButton.setStyleSheet("background-color : none;")
+        secPyButton.setStyleSheet("background-color : none")
+
+        # thirdPyButton = DragDrop()
+        # thirdPyButton.resize(180, 180)
+        # thirdPyButton.move(215, 5)
 
         thirdPyButton = QPushButton("Drag & Drop", self)
         thirdPyButton.clicked.connect(self.clickMethod)
         thirdPyButton.resize(180, 180)
         thirdPyButton.move(215, 5)
-        thirdPyButton.setStyleSheet("background-color : white;")
+        thirdPyButton.setStyleSheet("background-color : white")
 
         forthPyButton = QPushButton("Start", self)
         forthPyButton.clicked.connect(self.clickMethod)
         forthPyButton.resize(205, 32)
         forthPyButton.move(5, 42)
-        forthPyButton.setStyleSheet("background-color : none;")
+        forthPyButton.setStyleSheet("background-color : none")
 
     def clickMethod(self):
         print('Clicked Pyqt button.')
@@ -43,6 +105,9 @@ class MainWindow(QMainWindow):
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
+
+# chama a janela do progrma
     mainWin = MainWindow()
     mainWin.show()
-    sys.exit(app.exec_())
+
+    sys.exit(app.exec())
