d3e099f42e87c3ad25de8004378d5250253fce73
David Carrijø <49088508+davecarrijo@users.noreply.github.com>
2023-09-07 00:53:14 +0100

    Create FLipConvert.py

---
+++ FLipConvert.py
@@ -0,0 +1,55 @@
+# importing the required modules
+import PyPDF2
+
+def PDFrotate(origFileName, newFileName, rotation):
+
+    # creating a pdf File object of original pdf
+    pdfFileObj = open(origFileName, 'rb')
+
+    # creating a pdf Reader object
+    pdfReader = PyPDF2.PdfReader(pdfFileObj)
+
+    # creating a pdf writer object for new pdf
+    pdfWriter = PyPDF2.PdfWriter()
+
+    # rotating each page
+    for page in range(len(pdfReader.pages)):
+
+        # creating rotated page object
+        pageObj = pdfReader.pages[page]
+        pageObj.rotate(rotation)
+        pageObj.rotate(rotation)
+
+        # adding rotated page object to pdf writer
+        pdfWriter.add_page(pageObj)
+
+        # new pdf file object
+        newFile = open(newFileName, 'wb')
+
+        # writing rotated pages to new file
+        pdfWriter.write(newFile)
+
+    # closing the original pdf file object
+    pdfFileObj.close()
+
+    # closing the new pdf file object
+    newFile.close()
+
+
+def main():
+
+    # original pdf file name
+    origFileName = 'example.pdf'
+
+    # new pdf file name
+    newFileName = 'rotated_example.pdf'
+
+    # rotation angle
+    rotation = 270
+
+    # calling the PDFrotate function
+    PDFrotate(origFileName, newFileName, rotation)
+
+if __name__ == "__main__":
+    # calling the main function
+    main()
